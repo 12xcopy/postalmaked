1f42f8ffd53e7f75cee1e0bdb997fd1c22b44d50
12xcopy <moliroli00@gmail.com>
2025-09-24 13:52:41 -0500

    depth buffer works finally

--- psauce/gltest.c
+++ psauce/gltest.c
@@ -6,23 +6,28 @@
 
 #include "gltest.h"
 #include "postalrenwrap.h"
-//note: animations have a GetAtTime 
+// note: animations have a GetAtTime
 
-extern struct gltest_point_s*  ms_pPts2;
+extern struct gltest_point_s *ms_pPts2;
 
 typedef struct gltest_point_s pt_t;
-//gltest. serves to test opengl while also having the regular software renderer window on the side
+// gltest. serves to test opengl while also having the regular software renderer window on
+// the side
 
-
-static SDL_Window* two;
-static void* glx;
+static SDL_Window *two;
+static void *glx;
 void gltest_init()
 {
-	two = SDL_CreateWindow("GL Test",8,8,400,300,SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	two = SDL_CreateWindow("GL Test",
+	    8,
+	    8,
+	    400,
+	    300,
+	    SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 	SDL_ShowWindow(two);
 	glx = SDL_GL_CreateContext(two);
-	SDL_GL_MakeCurrent(two,glx);
-	glClearColor(0.4,0,0,1.0);
+	SDL_GL_MakeCurrent(two, glx);
+	glClearColor(0.4, 0, 0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 // copy of internal palette
@@ -30,41 +35,57 @@ uint8_t PAL_r[256];
 uint8_t PAL_g[256];
 uint8_t PAL_b[256];
 
+int DEPTH_MODE = GL_GREATER;
+int DEPTH_DRAW = 0;
+
 void gltest_preparerender()
 {
 	if (!two)
 	{
 		gltest_init();
 	}
-	SDL_GL_MakeCurrent(two,glx);
-	glClearColor(0.4,0,0,1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+	SDL_GL_MakeCurrent(two, glx);
+	glClearColor(0.4, 0, 0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// disable backface culling
 	glDisable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);  
+	glClearDepth(0.0);
 
-	psaucGetPaletteEntries(0,256,PAL_r,PAL_g,PAL_b,1);
+	if (DEPTH_DRAW)
+	{
+		glDepthFunc(GL_ALWAYS);
+	}
+	else
+	{
+		glDepthFunc(DEPTH_MODE);
+	}
+
+	psaucGetPaletteEntries(0, 256, PAL_r, PAL_g, PAL_b, 1);
 }
 
 void gltest_postrender()
 {
 	SDL_GL_SwapWindow(two);
-	int w,h;
-	SDL_GetWindowSize(two,&w,&h);
-	//actually the viewport might want to be moved since postal doesnt render at 0,0
-	glViewport(0,0,w,h);
+	int w, h;
+	SDL_GetWindowSize(two, &w, &h);
+	// actually the viewport might want to be moved since postal doesnt render at 0,0
+	glViewport(0, 0, w, h);
 }
 
 struct renderdata
 {
-	struct gltest_meshmeta_s* meta;
+	struct gltest_meshmeta_s *meta;
 };
 typedef uint32_t ARGB_t;
 
 union ARGB_u
 {
 	uint8_t v[4];
-	struct {
+	struct
+	{
 		uint8_t a;
 		uint8_t r;
 		uint8_t g;
@@ -87,13 +108,13 @@ ARGB_t palindex_to_argb(int index)
 	un.col.r = PAL_r[index];
 	un.col.g = PAL_g[index];
 	un.col.b = PAL_b[index];
-	return *((ARGB_t*)(&un));
+	return *((ARGB_t *)(&un));
 }
 
 struct glcolor_s argb_to_glcolor(ARGB_t c)
 {
 	struct glcolor_s color;
-	union ARGB_u un = *(union ARGB_u*)&c;
+	union ARGB_u un = *(union ARGB_u *)&c;
 
 	uint8_t red = un.col.r;
 	uint8_t grn = un.col.g;
@@ -107,30 +128,56 @@ struct glcolor_s argb_to_glcolor(ARGB_t c)
 	return color;
 }
 
+float apply_zbufcap(float input)
+{
+	// depth buffer usually sits around 0 - 10000, but sometimes goes higher than that
+	const float zbuffer_cap = 10000.0f;
 
-//note: points are already in screen space
+	if (input < 0)
+	{
+		// what??
+		return 0.0;
+	}
+
+	// input = zbuffer_cap -> i = 0.5
+	// input = infinity -> i = 1.0
+	float i = input / (zbuffer_cap + input);
+
+	// i = 1.0 - i;
+
+	if (i < 0 || i > 1)
+	{
+		printf("%.2f out of range (0 .. 1). Input was %.1f\n", i, input);
+		return 1.0;
+	}
+
+	return i;
+}
+
+// note: points are already in screen space
 
 // This is to be converted to modern gl later
-void gltest_render_internal(int x, int y,struct gltest_mesh_s* mesh,struct renderdata rd)
+void gltest_render_internal(int x, int y, struct gltest_mesh_s *mesh,
+    struct renderdata rd)
 {
 	const int sw = 600;
 	const int sh = 480;
 
-	int trico = mesh->m_sNum;
-	uint16_t* indecies = mesh->m_pArray;
-	struct glcolor_s dcolor = {0.3,0.8,0.6,1.0};
+	int trico		= mesh->m_sNum;
+	uint16_t *indecies	= mesh->m_pArray;
+	struct glcolor_s dcolor = {0.3, 0.8, 0.6, 1.0};
 
 	int rawset_len = trico;
 
-	for (int i=0;i<rawset_len;i++)
+	for (int i = 0; i < rawset_len; i++)
 	{
 
-		int seeker = i*3;
+		int seeker = i * 3;
 
-		//note: this promotes a uint16 to int32
+		// note: this promotes a uint16 to int32
 		int p1 = indecies[seeker];
-		int p2 = indecies[seeker+1];
-		int p3 = indecies[seeker+2];
+		int p2 = indecies[seeker + 1];
+		int p3 = indecies[seeker + 2];
 
 		pt_t pt1 = ms_pPts2[p1];
 		pt_t pt2 = ms_pPts2[p2];
@@ -150,34 +197,52 @@ void gltest_render_internal(int x, int y,struct gltest_mesh_s* mesh,struct rende
 		pt3.v[0] /= (sw / 2.0f);
 		pt3.v[1] /= (sh / 2.0f);
 
+		pt1.v[2] = apply_zbufcap(pt1.v[2]);
+		pt2.v[2] = apply_zbufcap(pt2.v[2]);
+		pt3.v[2] = apply_zbufcap(pt3.v[2]);
+
 		if (rd.meta)
 		{
-			int entry = rd.meta->colors_elements[i];
+			int entry     = rd.meta->colors_elements[i];
 			ARGB_t pcolor = palindex_to_argb(entry);
-			dcolor = argb_to_glcolor(pcolor);
+			dcolor	      = argb_to_glcolor(pcolor);
 		}
 
 		glBegin(GL_TRIANGLES);
-		glColor3f(dcolor.r,dcolor.g,dcolor.b);
-		glVertex2f(pt1.v[0],-pt1.v[1]);
-		glVertex2f(pt2.v[0],-pt2.v[1]);
-		glVertex2f(pt3.v[0],-pt3.v[1]);
+
+		if (!DEPTH_DRAW)
+		{
+			glColor3f(dcolor.r, dcolor.g, dcolor.b);
+			glVertex3f(pt1.v[0], -pt1.v[1], pt1.v[2]);
+			glVertex3f(pt2.v[0], -pt2.v[1], pt2.v[2]);
+			glVertex3f(pt3.v[0], -pt3.v[1], pt3.v[2]);
+		}
+		else
+		{
+			glColor3f(pt1.v[2], pt1.v[2], pt1.v[2]);
+			glVertex3f(pt1.v[0], -pt1.v[1], pt1.v[2]);
+			glColor3f(pt2.v[2], pt2.v[2], pt2.v[2]);
+			glVertex3f(pt2.v[0], -pt2.v[1], pt2.v[2]);
+			glColor3f(pt2.v[2], pt2.v[2], pt2.v[2]);
+			glVertex3f(pt3.v[0], -pt3.v[1], pt3.v[2]);
+			glColor3f(pt2.v[2], pt2.v[2], pt2.v[2]);
+		}
+
 		glEnd();
 	}
-
 }
 
-
-void gltest_renderwm(int x, int y,struct gltest_mesh_s* mesh,struct gltest_meshmeta_s* meta)
+void gltest_renderwm(int x, int y, struct gltest_mesh_s *mesh,
+    struct gltest_meshmeta_s *meta)
 {
 	struct renderdata rd;
-	rd.meta=meta;
-	gltest_render_internal(x,y,mesh,rd);
+	rd.meta = meta;
+	gltest_render_internal(x, y, mesh, rd);
 }
 
-void gltest_render(int x, int y,struct gltest_mesh_s* mesh)
+void gltest_render(int x, int y, struct gltest_mesh_s *mesh)
 {
 	struct renderdata rd;
-	rd.meta=0;
-	gltest_render_internal(x,y,mesh,rd);
+	rd.meta = 0;
+	gltest_render_internal(x, y, mesh, rd);
 }
